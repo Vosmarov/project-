dd9290845b21d8b609d50ff7defc394160d0dda5
Vosmarov <denistikhonov.82@gmail.com>
2020-08-28 21:51:45 +0900

    ыв

--- Вебинар 1 дз/homework 26.08.20 (6).py
+++
@@ -1,7 +0,0 @@
-training_max_results = 3.3
-training_days = 1
-result_day = float(input('Введите результат первого забега [км]'))
-while result_day < training_max_results:
-    print(f'{training_days} - й день: {result_day} км')
-    result_day = result_day / 10 + result_day
-    training_days += 1
