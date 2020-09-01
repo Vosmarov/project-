e0628fd3ab99f47bf412f7e6bf9ffee0948670a0
Vosmarov <denistikhonov.82@gmail.com>
2020-09-01 20:55:06 +0900

    Deleted HW-1

--- Вебинар 1 дз/homework 26.08.20 (1).py
+++
@@ -1,3 +0,0 @@
-username = input('Введите ваше имя')
-age = int(input('Введите ваш возраст'))
-print(f'Привет, {username}, тебе {age} лет!')
--- Вебинар 1 дз/homework 26.08.20 (2).py
+++
@@ -1,5 +0,0 @@
-user_seconds = int(input('Введите количество секунд'))
-hour = user_seconds // 3600
-minutes = (user_seconds % 3600) // 60
-seconds = (user_seconds % 3600) % 60
-print(hour, minutes, seconds)
